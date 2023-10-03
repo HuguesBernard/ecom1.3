51bd24f4eefbd5b2946335b238d89b2ee807b0b8
HuguesBernard <jhouguebern01@gmail.com>
2023-10-03 13:57:32 -0400

    ajout

---
+++ function.php
@@ -0,0 +1,21 @@
+<?php
+function nameLengthIsValid($nameToValid){
+    //minimum 2 max 10
+    $length = strlen($nameToValid);
+    $responses=['isValid'=>true,
+    'msg'=>''
+    ];
+    if ($length <2) {
+        $responses=['isValid'=>false,
+        'msg'=>'Nom trop court'
+    ];
+    }
+    else if ($length >10) {
+        $responses=['isValid'=>false,
+        'msg'=>'Nom trop long'
+    ];
+    }
+    return $responses;
+
+}
+?>
--- result.php
+++ result.php
@@ -1,3 +1,20 @@
-<h1 >
-    Ma page est resultat resultat
-</h1>
+<?php
+require_once("function.php");
+var_dump($_POST);
+
+if ($_POST) {
+    $name= $_POST['fname'];
+    if (empty($name)) {
+        echo "</br>Nom vide";
+    }
+    else {
+        echo "</br>Mon nom est:" . $name;
+    }
+$nameLengthIsValid = nameLengthIsValid($_POST['fname']);
+echo'</br>';
+var_dump($nameLengthIsValid);
+if ($nameLengthIsValid['isValid']) {
+    
+}
+}
+?>
